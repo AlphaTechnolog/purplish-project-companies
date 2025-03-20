8c841686df5c1d6f65e9de0709581686fe7a85c5
Gabriel Guerra <gfranklings@gmail.com>
2025-03-20 08:52:55 -0400

    chore: Run go fmt

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "github.com/alphatechnolog/purplish-project-companies/core"
-    "github.com/alphatechnolog/purplish-project-companies/database"
-    "github.com/gin-gonic/gin"
+	"github.com/alphatechnolog/purplish-project-companies/core"
+	"github.com/alphatechnolog/purplish-project-companies/database"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    db, err := database.OpenDBConnection()
-    if err != nil {
-        log.Fatal("A fatal error ocurred when opening db", err)
-        return
-    }
-    defer db.Close()
+	db, err := database.OpenDBConnection()
+	if err != nil {
+		log.Fatal("A fatal error ocurred when opening db", err)
+		return
+	}
+	defer db.Close()
 
-    r := gin.Default()
-    defer r.Run()
+	r := gin.Default()
+	defer r.Run()
 
-    core.CreateCompaniesRoutes(db, r.Group("/companies"))
+	core.CreateCompaniesRoutes(db, r.Group("/companies"))
 }
