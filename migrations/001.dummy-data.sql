6791a495a350cbef9a3361a931baaa05509d5711
Gabriel Guerra <gfranklings@gmail.com>
2025-03-21 17:43:22 -0400

    feat: Add secondary company to migrations

--- migrations/001.dummy-data.sql
+++ migrations/001.dummy-data.sql
@@ -1,5 +1,3 @@
-INSERT INTO companies (id, name, status) VALUES (
-    'b918deaf-92ab-485d-9a69-ee7a2a5f4aef',
-    'Primary company',
-    1
-)
+INSERT INTO companies (id, name, description, status) VALUES
+    ('b918deaf-92ab-485d-9a69-ee7a2a5f4aef', 'Primary company', 'Premium company', 1),
+    ('cde98fd2-6023-4953-9e95-884aed1f09ce', 'Secondary company', 'Free tier company', 1);
